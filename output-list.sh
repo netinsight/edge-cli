12cc54cf0a70b3d482c587d0451e895088d3aaf8
Linus <linus.wallgren@netinsight.net>
2023-05-11 12:49:47 +0200

    Include health in input and output health

--- output-list.sh
+++ output-list.sh
@@ -5,12 +5,16 @@ set -euo pipefail
 edge_url="${EDGE_URL?:missing environment variable: EDGE_URL}"
 cookie_jar="$("$(dirname -- "${BASH_SOURCE[0]}")/login.sh" "$edge_url")"
 
-outputs="$(curl "$edge_url/api/output/" \
+
+while read -r id name health_state health_title ; do
+    if [ "$health_state" == "allOk" ]; then
+        health="\e[32m✓\e[0m"
+    else
+        health="\e[31m✗\e[0m"
+    fi
+    printf "%s %s $health: %s\n" "$id" "$name" "$health_title"
+done < <(curl "$edge_url/api/output/" \
     --silent \
     --get \
-    --cookie "$cookie_jar")"
-
-(
-    printf "Name\tid\n" &&
-    jq --raw-output '.items | map([.name, .id])[] | @tsv' <<<"$outputs"
-) | column -t
+    --cookie "$cookie_jar" \
+    | jq --raw-output '.items | map([.id, .name, .health.state, .health.title])[] | @tsv')
