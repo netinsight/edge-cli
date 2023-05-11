6e7098c07addd7cbd93ee3c8787ffb48c46a5c05
Linus <linus.wallgren@netinsight.net>
2023-05-11 12:01:02 +0200

    refresh token when it has expired

--- login.sh
+++ login.sh
@@ -7,6 +7,17 @@ install_name="${install_name#http://}"
 install_name="${install_name#/}"
 
 cookie_jar="$HOME/.config/edge-cli/$install_name.cookie"
+
+if [ -f "$cookie_jar" ]; then
+    now="$(date +%s)"
+    # parse the curl cookie file, reference: https://curl.se/docs/http-cookies.html
+    expires="$(awk "/$install_name/"' { print $5 }' "$cookie_jar")"
+    if (( "$expires" < "$now" )); then
+        echo >&2 "Cookie has expired, refreshing"
+        rm "$cookie_jar"
+    fi
+fi
+
 if ! [ -f "$cookie_jar" ]; then
     mkdir -p "$(dirname "$cookie_jar")"
     curl "$edge_url/api/login/" \
