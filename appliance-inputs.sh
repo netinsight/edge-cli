2825745ee41ace489352f22d6c271f8e62963334
Linus <linus.wallgren@netinsight.net>
2023-09-13 10:22:26 +0200

    Use input/output list for appliance inputs/outputs
    This is in order to support older edge versions which did not yet have
    the appliance/*/inputs and appliance/*/outputs endpoints

--- appliance-inputs.sh
+++ appliance-inputs.sh
@@ -7,12 +7,13 @@ cookie_jar="$("$(dirname -- "${BASH_SOURCE[0]}")/login.sh" "$edge_url")"
 
 id="${1?missing argument: input ID}"
 
-inputs="$(curl "$edge_url/api/appliance/$id/inputs" \
+inputs="$(curl "$edge_url/api/input/" \
     --silent \
     --get \
+    --data-urlencode 'q={"filter":{"applianceId":"'"$id"'"}}' \
     --cookie "$cookie_jar")"
 
 (
     printf "ID\tName\n" &&
-    jq --raw-output '.items | map([.inputId, .inputName])[] | @tsv' <<<"$inputs"
+    jq --raw-output '.items | map([.id, .name])[] | @tsv' <<<"$inputs"
 ) | column -t
--- appliance-outputs.sh
+++ appliance-outputs.sh
@@ -5,14 +5,15 @@ set -euo pipefail
 edge_url="${EDGE_URL?:missing environment variable: EDGE_URL}"
 cookie_jar="$("$(dirname -- "${BASH_SOURCE[0]}")/login.sh" "$edge_url")"
 
-id="${1?missing argument: input ID}"
+id="${1?missing argument: output ID}"
 
-outputs="$(curl "$edge_url/api/appliance/$id/outputs" \
+outputs="$(curl "$edge_url/api/output/" \
     --silent \
     --get \
+    --data-urlencode 'q={"filter":{"appliance":"'"$id"'","searchName":""}}' \
     --cookie "$cookie_jar")"
 
 (
     printf "ID\tName\n" &&
-    jq --raw-output '.items | map([.outputId, .outputName])[] | @tsv' <<<"$outputs"
+    jq --raw-output '.items | map([.id, .name])[] | @tsv' <<<"$outputs"
 ) | column -t
