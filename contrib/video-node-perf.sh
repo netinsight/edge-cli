dd0b5c45519ddde92c8d7c26479c2ed9b499a523
Linus <linus.wallgren@netinsight.net>
2023-09-14 22:07:55 +0200

    Let video-node-perf skip existing inputs and outputs
    The assumption is that they are already correctly configured and if one
    wishes to reconfigure them one can simply delete all

--- contrib/video-node-perf.sh
+++ contrib/video-node-perf.sh
@@ -8,15 +8,28 @@
 # under test. It will simply discard all incoming streams
 
 generator_appliance="${1?missing argument: Generator appliance}"
-output_appliance="${1?missing argument: Output appliance}"
+output_appliance="${2?missing argument: Output appliance}"
+num_outputs="${3?missing argument: number of outputs}"
 
 set -e
-set -x
 
-edge input create "Perftest-0-sdi" --appliance "$generator_appliance" --interface av1 --mode sdi 
-edge output create "Perftest-0-multicast" --appliance "$generator_appliance" --interface lo --mode rtp --source 127.0.0.1 --dest "224.0.0.44:4444" --input "Perftest-0-sdi"
+inputs="$(edge input list | awk '/^ID/ { next } { print $2 }')"
+outputs="$(edge output list | awk '/^ID/ { next } { print $2 }')"
 
-for i in {1..50}; do 
-    edge input create   "Perftest-$i-RTP_input"  --appliance "$generator_appliance" --interface lo --mode rtp --port 4444 --multicast 224.0.0.44
-    edge output create  "Perftest-$i-UDP_output" --appliance "$output_appliance" --interface lo --mode udp --dest "127.0.0.1:1234" --input "Perftest-$i-RTP_input"
+if ! grep -qw Perftest-0-sdi <<<"$inputs"; then
+    edge input create "Perftest-0-sdi" --appliance "$generator_appliance" --interface av1 --mode sdi --disable-thumbnails
+fi
+if ! grep -qw Perftest-0-multicast <<<"$outputs"; then
+    edge output create "Perftest-0-multicast" --appliance "$generator_appliance" --interface lo --mode rtp --source 127.0.0.1 --dest "224.0.0.44:4444" --input "Perftest-0-sdi"
+fi
+
+for i in $(seq 1 "$num_outputs"); do
+    input="Perftest-$i-RTP_input"
+    output="Perftest-$i-UDP_output"
+    if ! grep -qw "$input" <<<"$inputs"; then
+        edge input create   "$input"  --appliance "$generator_appliance" --interface lo --mode rtp --port 4444 --multicast 224.0.0.44
+    fi
+    if ! grep -qw "$output" <<<"$outputs"; then
+        edge output create  "$output" --appliance "$output_appliance" --interface lo --mode udp --dest "127.0.0.1:1234" --input "$input"
+    fi
 done
