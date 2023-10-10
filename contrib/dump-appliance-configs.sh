ad11cabb10573808330450fbaa1ffb0ab1094662
Linus <linus.wallgren@netinsight.net>
2023-10-10 14:53:03 +0200

    contrib: Add small doc on how to use dump-appliance-config

--- contrib/dump-appliance-configs.sh
+++ contrib/dump-appliance-configs.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-cookie_jar="$("$(dirname -- "${BASH_SOURCE[0]}")/../login.sh" "$EDGE_URL")"
+# Usage:
+# ./$0 appliance-config-1
+# # Do some change
+# ./$0 appliance-config-2
+# diff -qr appliance-config-1 appliance-config-2
 
 out=${1?missing argument: output directory}
+
+cookie_jar="$("$(dirname -- "${BASH_SOURCE[0]}")/../login.sh" "$EDGE_URL")"
+
 mkdir -p "$out"
 for appliance in $(edge appliance list | awk 'NR == 1 { next } { print $2 }'); do
     echo "Fetching appliance config for appliance $appliance" >&2
-    curl --silent "$EDGE_URL/api/appliance/$appliance/config" --get --cookie "$cookie_jar" > "$out/$appliance.json"
+    curl --silent "$EDGE_URL/api/appliance/$appliance/config" --get --cookie "$cookie_jar" | jq --sort-keys . > "$out/$appliance.json"
 done
