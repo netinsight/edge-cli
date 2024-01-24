8e37c10c227b42ea5dfe2cbf8b9534944c89bffe
Linus <linus.wallgren@netinsight.net>
2024-01-24 16:33:16 +0100

    Include appliance secret in group list

--- group-list.sh
+++ group-list.sh
@@ -6,12 +6,12 @@ edge_url="${EDGE_URL?:missing environment variable: EDGE_URL}"
 cookie_jar="$("$(dirname -- "${BASH_SOURCE[0]}")/login.sh" "$edge_url")"
 
 (
-    printf "%s\t%s\n" "ID" "Name"
+    printf "%s\t%s\t%s\n" "ID" "Name" "Secret"
     while read -r id name ; do
-        printf "%s\t%s\n" "$id" "$name"
+        printf "%s\t%s\t%s\n" "$id" "$name"
     done < <(curl "$edge_url/api/group/" \
         --silent \
         --get \
         --cookie "$cookie_jar" \
-        | jq --raw-output '.items | map([.id, .name])[] | @tsv')
+        | jq --raw-output '.items | map([.id, .name, .applianceSecret])[] | @tsv')
 ) | column -t
