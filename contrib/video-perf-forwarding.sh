f22f2af299e1ce9e3dbf349b8bfa971a91553450
Linus <linus.wallgren@netinsight.net>
2025-03-07 11:25:44 +0100

    contrib: Support setting bitrate and protocol

--- contrib/video-perf-forwarding.sh
+++ contrib/video-perf-forwarding.sh
@@ -8,12 +8,26 @@
 # stream isn't routed via a core node.
 
 bitrate="$((20 * 1000000))"
+test_appliance=dut
+protocol=srt
 
 while [[ $# -gt 0 ]]; do
     case $1 in
         -v | --verbose)
             shift
             set -x
+            ;;
+        --bitrate)
+            bitrate=$(numfmt --from=auto "${2}")
+            shift 2
+            ;;
+        --test-appliance)
+            test_appliance="$2"
+            shift 2
+            ;;
+        --protocol)
+            protocol="$2"
+            shift 2
             ;;
         -*)
             echo "Unknown option $1"
@@ -31,13 +45,10 @@ set -euo pipefail
 
 generator_appliance=$(edge appliance list | awk '$3 == "edgeConnect" && $1 ~ /input/ { print $1 }' | sort | head)
 output_appliance=$(edge appliance list | awk '$3 == "edgeConnect" && $1 ~ /output/ { print $1 }' | sort | head)
-test_appliance=edge-194
 
-generator_interface=$(edge appliance show "$generator_appliance" | awk '/^Interface.*public/ { print $2 }')
-output_interface=$(edge appliance show "$output_appliance" | awk '/^Interface.*public/ { print $2 }')
-output_ip=$(edge appliance show "$output_appliance" | awk '/^Interface.*public/ { print $6 }') # the public ip
-test_interface=$(edge appliance show "$test_appliance" | awk '/^Interface.*public/ { print $2 }')
-test_ip=$(edge appliance show "$test_appliance" | awk '/^Interface.*public/ { print $6 }') # the public ip
+generator_interface=$(edge appliance show "$generator_appliance" | awk '/Name:/ { name=$3; found=0 } /Networks:.*streaming/ { found=1 } /Address:/ && found { print(name) }')
+read -r output_interface output_ip <<< "$(edge appliance show "$output_appliance" | awk '/Name:/ { name=$3; found=0 } /Networks:.*streaming/ { found=1 } /Address:/ && found { print(name, $3) }')"
+read -r test_interface test_ip <<< "$(edge appliance show "$test_appliance" | awk '/Name:/ { name=$3; found=0 } /Networks:.*streaming/ { found=1 } /Address:/ && found { print(name, $3) }')"
 
 cat >&2 <<EOF
 Generator appliance:    ${generator_appliance}
@@ -53,44 +64,73 @@ echo >&2 "Setting up generators"
 if ! grep -qw "Perftest-generator-$generator_appliance" <<<"$inputs"; then
     edge input create "Perftest-generator-$generator_appliance" \
         --appliance "$generator_appliance" \
-        --interface lo \
         --mode generator \
         --bitrate "$bitrate" \
-        --disable-thumbnails
+        --thumbnail none
 fi
 if ! grep -qw "Perftest-generator-$generator_appliance" <<<"$outputs"; then
     edge output create "Perftest-generator-$generator_appliance" \
         --input "Perftest-generator-$generator_appliance" \
         --appliance "$generator_appliance" \
         --interface lo \
-        --mode rtp \
         --source 127.0.0.1 \
+        --mode rtp \
         --dest 224.0.0.44:4444 
 fi
 
 for i in $(seq "$num_outputs"); do
     gen_in="Perftest-$i-s1-RTP_gen_input"
     if ! grep -qw "$gen_in" <<<"$inputs"; then
-        edge input create "$gen_in" --appliance "$generator_appliance" --interface lo --mode rtp --port 4444 --multicast 224.0.0.44 --disable-thumbnails
+        edge input create "$gen_in" --appliance "$generator_appliance" --interface lo --mode rtp --port 4444 --multicast 224.0.0.44 --thumbnail none
     fi
 
-    gen_out="Perftest-$i-s2-RTP_gen_output"
-    if ! grep -qw "$gen_out" <<<"$outputs"; then
-        edge output create "$gen_out" --appliance "$generator_appliance" --interface "$generator_interface" --mode rtp --dest "$test_ip:$((4000 + i*6))" --input "$gen_in"
-    fi
+    case $protocol in
+        rtp)
+            gen_out="Perftest-$i-s2-RTP_gen_output"
+            if ! grep -qw "$gen_out" <<<"$outputs"; then
+                edge output create "$gen_out" --appliance "$generator_appliance" --interface "$generator_interface" --mode rtp --dest "$test_ip:$((4000 + i*6))" --input "$gen_in"
+            fi
 
-    input="Perftest-$i-s3-RTP_input"
-    if ! grep -qw "$input" <<<"$inputs"; then
-        edge input create "$input" --appliance "$test_appliance" --interface "$test_interface" --mode rtp --port "$((4000 + i*6))" --thumbnails edge
-    fi
+            input="Perftest-$i-s3-RTP_input"
+            if ! grep -qw "$input" <<<"$inputs"; then
+                edge input create "$input" --appliance "$test_appliance" --interface "$test_interface" --mode rtp --port "$((4000 + i*6))" --thumbnail edge
+            fi
 
-    output="Perftest-$i-s4-RTP_output"
-    if ! grep -qw "$output" <<<"$outputs"; then
-        edge output create "$output" --appliance "$test_appliance" --interface "$test_interface" --mode rtp --dest "$output_ip:$((4000 + i*6))" --input "$input"
-    fi
+            output="Perftest-$i-s4-RTP_output"
+            if ! grep -qw "$output" <<<"$outputs"; then
+                edge output create "$output" --appliance "$test_appliance" --interface "$test_interface" --mode rtp --dest "$output_ip:$((4000 + i*6))" --input "$input"
+            fi
 
-    tr101290_input="Perftest-$i-s5-RTP_input_tr101290"
-    if ! grep -qw "$tr101290_input" <<<"$inputs"; then
-        edge input create "$tr101290_input" --appliance "$output_appliance" --interface "$output_interface" --mode rtp --port "$((4000 + i*6))" --disable-thumbnails
-    fi
+            tr101290_input="Perftest-$i-s5-RTP_input_tr101290"
+            if ! grep -qw "$tr101290_input" <<<"$inputs"; then
+                edge input create "$tr101290_input" --appliance "$output_appliance" --interface "$output_interface" --mode rtp --port "$((4000 + i*6))" --thumbnail none
+            fi
+
+            ;;
+        srt)
+            gen_out="Perftest-$i-s2-SRT_gen_output"
+            if ! grep -qw "$gen_out" <<<"$outputs"; then
+                edge output create "$gen_out" --appliance "$generator_appliance" --interface "$generator_interface" --mode srt --caller --dest "$test_ip:$((4000 + i*6))" --input "$gen_in"
+            fi
+
+            input="Perftest-$i-s3-SRT_input"
+            if ! grep -qw "$input" <<<"$inputs"; then
+                edge input create "$input" --appliance "$test_appliance" --interface "$test_interface" --mode srt --listener --port "$((4000 + i*6))" --thumbnail edge
+            fi
+
+            output="Perftest-$i-s4-SRT_output"
+            if ! grep -qw "$output" <<<"$outputs"; then
+                edge output create "$output" --appliance "$test_appliance" --interface "$test_interface" --mode srt --caller --dest "$output_ip:$((4000 + i*6))" --input "$input"
+            fi
+
+            tr101290_input="Perftest-$i-s5-SRT_input_tr101290"
+            if ! grep -qw "$tr101290_input" <<<"$inputs"; then
+                edge input create "$tr101290_input" --appliance "$output_appliance" --interface "$output_interface" --mode srt --listener --port "$((4000 + i*6))" --thumbnail none
+            fi
+            ;;
+        *)
+            echo >&2 "Unknown protocol $protocol"
+            exit 1
+            ;;
+    esac
 done
